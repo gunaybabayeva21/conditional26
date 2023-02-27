9c25f770c9745f031b5af4a4ca46244b0e0b1fb8
gunaybabayeva21 <gunaybabayeva032@gmail.com>
2023-02-27 04:10:02 +0400

    new project

---
+++ Conditionaltask26/Program.cs
@@ -0,0 +1,131 @@
+﻿//// Task1 
+
+//int num = 74;
+
+//if (num ==0)
+
+//{
+//    Console.WriteLine("ne menfi ne musbet ededdir ");
+//}
+//else if (num > 0)
+//{
+//    Console.WriteLine("musbet ededdir");
+//}
+//else
+//{
+//    Console.WriteLine("menfi eddedir");
+//}
+
+
+
+
+//Task2
+
+//int num = 34;
+
+//if (num== 0)
+//{
+//    Console.WriteLine("Ne tek ne cut");
+
+//}
+//else if (num % 2 == 0)
+//{
+//    Console.WriteLine("cut ededdir");
+//}
+//else
+//{
+//    Console.WriteLine("Tek ededdir");
+
+//}
+
+
+
+
+////Task3
+
+//int year = 1990;
+//if ((year += 18) > 2023)
+//{
+//    Console.WriteLine("18 tamam olub");
+//}
+//else
+//{
+//    Console.WriteLine("18 tamam olmayib");
+//}
+    
+
+
+
+
+
+
+
+
+
+
+//Task4
+
+//int A = 4;
+//int B = 8;
+//int C = 9;
+//int sum = 0;
+//if (A > 5)
+//{
+//    Console.WriteLine( sum+=A);
+//}
+
+//if (B > 5)
+//{ Console.WriteLine( sum+=B);
+
+//}
+
+//if (C > 5)
+// {
+//     Console.WriteLine(sum+=C);
+// }
+
+//Console.WriteLine(sum);
+
+
+
+
+
+//Task5 
+
+//int num = 456;
+
+//Console.WriteLine(num / 100);
+
+    
+ 
+    
+
+
+
+
+
+
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+            
+
+
+
+
+
+
+
+
